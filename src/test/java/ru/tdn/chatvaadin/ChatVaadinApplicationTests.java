55f8db28a9430a6619f5e4077e66dd1234f40abc
Dmitry Tischenko <tdmitriu@mail.ru>
2021-12-05 20:09:09 +0300

    first commit

---
+++ src/test/java/ru/tdn/chatvaadin/ChatVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tdn.chatvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatVaadinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
